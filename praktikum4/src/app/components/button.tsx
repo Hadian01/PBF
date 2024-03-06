edc81a46c5e5c74e454c6b983c196ac73b81ebf5
Hadian01 <hadiannelvi82@gmail.com>
2024-03-06 10:06:44 +0700

    Praktikum2_eventand state

--- praktikum4/src/app/components/button.tsx
+++ praktikum4/src/app/components/button.tsx
@@ -1,22 +1,26 @@
+export function Tombol_2({ isiPesan, namaTombol }: { isiPesan: string, namaTombol: string }) {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={() => alert(isiPesan)}>
+            {namaTombol}
+        </button>
+    );
+}
+
 export default function Tombol_l() {
     function HandleClick() {
-        alert("Tombol telah di tekan!");
+        alert("Tombol telah ditekan!");
     }
 
-    function HandleMouseOver() {
-        alert("Eits, mau mencet tombol ya?");
+    function HandleMouseLeave() {
+        alert("Loh, kok sudah pergi!");
     }
 
     return (
-    <button className= "bg-blue-500 hover: bg-blue-700 text-white p-2 rounded"
-    onClick={HandleClick}
-    //onMouseOver={HandleMouseOver}
-    onMouseLeave={() => {
-        alert("Loh, kok sudah pergi!")
-    }
-}
-    >
-    Ini tombol
-    </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+            onClick={HandleClick}
+            onMouseLeave={HandleMouseLeave}>
+            Ini tombol
+        </button>
     );
 }
