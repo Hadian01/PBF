3ba966b01ee8c9492755bfb4ac1c0300df030d13
Hadian01 <hadiannelvi82@gmail.com>
2024-03-06 22:13:22 +0700

    Praktikum 5

--- praktikum4/src/app/page.tsx
+++ praktikum4/src/app/page.tsx
@@ -1,23 +1,28 @@
 "use client";
-import Tombol_l, { Tombol_2, Tombol_3 } from "./components/button";
+import Tombol_1, { Tombol_2, Tombol_3 } from "./components/button";
 import Gallery from "./components/gallery";
+import Form, { Form_2 } from "./components/form";
 
-export default function Home() {
-return (
-  <>
-  <div className="container mx-auto">
+export default function Home(){
+  return(
+    <>
+    <div className="container mx-auto">
     <h2>Kuis Kota</h2>
-    <Tombol_l />
+    <Tombol_1/>
     <hr></hr>
-    <Tombol_2 isiPesan="Ini Pesanku" namaTombol="Pesan" />
-  </div>
-  <hr></hr>
-  <div className="bg-red-300" onClick={() => alert('Parent Element : Div')}>
-    <Tombol_3 isiPesan="Child Element : Tombol-1" namaTombol="Tombol-1" />
-    <Tombol_3 isiPesan="Child Element : Tombol-2" namaTombol="Tombol-2" />
-  </div>
-  <br></br>
-  <Gallery />
-  </>
-);
+    <Tombol_2 isiPesan="Ini Pesanku" namaTombol="Pesan"/>
+    </div>
+    <br></br>
+    <div className="bg-red-300" onClick={() => alert('Parent Element : Div')}>
+      <Tombol_3 isiPesan="Child Element : Tombol-1" namaTombol="Tombol-1"/>
+      <Tombol_3 isiPesan="Child Element : Tombol-2" namaTombol="Tombol-2"/>
+    </div>
+    <br></br>
+    <Gallery/>
+    <br></br>
+    <Form/>
+    <br></br>
+    <Form_2 />
+    </>
+  );
 }
