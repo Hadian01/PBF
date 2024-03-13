41be15a21614b1bf9ea67ba716edc3b3140eee16
Hadian01 <hadiannelvi82@gmail.com>
2024-03-13 10:22:06 +0700

    praktikum1

---
+++ praktikum5/src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+export default function Profile() {
+    return ( 
+    <div>
+        <h1>Biodata saya:</h1> 
+            <ul>
+                <li><strong>Nama :</strong>Hadian Nelvi</li>
+                <li><strong>Nim :</strong>2341728025</li>
+                <li><strong>Kelas :</strong>TI 3 C</li>
+                <li><strong>Jurusan :</strong>TEKNOLOGI INFORMASI</li>
+                <li><strong>Umur :</strong>17</li>
+            </ul> 
+    </div>
+    );
+}
